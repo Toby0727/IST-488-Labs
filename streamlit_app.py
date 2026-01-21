78a222d95e23bde518f7bf1b3a96f0ae7d874a60
Toby0727 <hpang05@syr.edu>
2026-01-21 18:58:46 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,15 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.set_page_config(page_title="Lab App", layout="wide")
+
+# Create pages
+pages = {
+    "Lab 1": st.Page("Lab1.py", title="Lab 1"),
+    "Lab 2": st.Page("Lab2.py", title="Lab 2"),
+}
+
+# Default page = Lab2
+pg = st.navigation(pages, position="sidebar", default="Lab 2")
+
+pg.run()
