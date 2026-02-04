839d44c7a4b1d1fd1ab31c69779bfaafd6e0b23c
Toby0727 <hpang05@syr.edu>
2026-02-04 18:53:17 +0000

    lab 3 toby

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import openai as openai
 lab1 = st.Page('labs/lab1.py' , title = 'lab 1')
 lab2 = st.Page('labs/lab2.py' , title = 'lab 2', default=True)
 lab3 = st.Page('labs/lab3.py' , title = 'lab 3')
-pg = st.navigation([lab1, lab2, lab3 ])
+lab3_toby = st.Page('labs/lab3.py' , title = 'lab 3 toby')
+pg = st.navigation([lab1, lab2, lab3, lab3_toby])
 st.set_page_config(page_title= 'IST 488 Lab', initial_sidebar_state='expanded')
 pg.run()
 
