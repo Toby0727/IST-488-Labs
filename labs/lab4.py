a73d806412f74b4a894b648de26926f50dd45807
Toby0727 <hpang05@syr.edu>
2026-02-11 19:04:57 +0000

    text

--- labs/lab4.py
+++ labs/lab4.py
@@ -65,6 +65,7 @@ if existing_count == 0:
                         model="text-embedding-3-small"  # OpenAI embeddings model
                     ).data[0].embedding
                     
+                    st.write(text_content[:200])
                     # Add to ChromaDB collection
                     collection.add(
                         documents=[text_content],      # The text
