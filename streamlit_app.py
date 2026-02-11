53bf4638b1bc01b7253e07092446da31bdc2c8d4
Toby0727 <hpang05@syr.edu>
2026-02-11 19:31:49 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ lab2 = st.Page('labs/lab2.py', title='lab 2')
 lab3 = st.Page('labs/lab3(Toby).py', title='lab 3')
 lab4 = st.Page('labs/lab4.py', title='lab 4', default=True)
 pg = st.navigation([lab1, lab2, lab3, lab4])
-pg.run()
+
 
